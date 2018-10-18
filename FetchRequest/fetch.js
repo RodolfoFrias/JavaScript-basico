38b2e8be7f5f1503dc897271d6a46f2213f37fc6
Bittmen <38054257+Bittmen@users.noreply.github.com>
2018-10-18 16:34:46 -0500

    Promesas y fetch

--- FetchRequest/fetch.js
+++ FetchRequest/fetch.js
@@ -1,5 +1,7 @@
 'use strict'
 
+//https://reqres.in/api/users  -> Hacer ejercicios con esta URI
+
 var div = document.querySelector("#items");
 var usuarios = [];
 
@@ -16,3 +18,16 @@ fetch('https://jsonplaceholder.typicode.com/posts')
             document.querySelector('.load').style.display = 'none';
         });
 });
+
+
+/*
+Para enviar datos por POST con fetch
+fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({a: 1, b: 'Cadena de texto'})
+  });*/
+
---
+++ Promises/promise.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function getInfo(){
+    var info = {
+        Nombre : "Juan",
+        Apellido : "Perez",
+        Edad : 22,
+        Ocupacion : "Software developer"
+    }
+
+    return new Promise((solve, reject) => {
+        
+    });
+
+}
